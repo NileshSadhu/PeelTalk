46d53eb46b36c7f2de2c9e28daeed61ef8c4f14a
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-05-20 00:06:20 +0530

    tailwind init frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
       <div>
       </div>
       <div>
-        <h1>Banana</h1>
-        <p>
+        <h1 className='font-bold text-4xl'>Banana</h1>
+        <p className='font-large text-xl text-yellow-400'>
           Banana - because talking to strangers is more fun than talking to your plants.
         </p>
       </div>
