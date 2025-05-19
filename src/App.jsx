141c103b13d8d10e69b88dc0e848f9f2914754b0
Nilesh <nileshsadhu640@gmail.com>
2025-05-19 13:08:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <div>
+        <h1>Banana</h1>
+        <p>
+          Banana - because talking to strangers is more fun than talking to your plants.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
