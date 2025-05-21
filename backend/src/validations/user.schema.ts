dac689d520df93e0f4b5356cf6e8cb972ea197f4
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-05-21 23:51:19 +0530

    feat: add forget and reset password controller APIs with OTP verification

--- backend/src/validations/user.schema.ts
+++ backend/src/validations/user.schema.ts
@@ -5,11 +5,16 @@ export const signupSchema = z.object({
     name: z.string().min(1),
     email: z.string().email(),
     username: z.string().min(3),
-    password: z.string().min(7)
+    password: z.string().min(8)
 })
 
 export const loginSchema = z.object({
     username: z.string().min(3).optional(),
     email: z.string().email().optional(),
-    password: z.string().min(7)
+    password: z.string().min(8)
+})
+
+export const passwordResetSchema = z.object({
+    email: z.string().email(),
+    Newpassword: z.string().min(8)
 })
