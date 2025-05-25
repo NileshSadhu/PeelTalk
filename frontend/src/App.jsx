03f7d24990bd82d742c49a08172ca824975d28e7
Nilesh <nileshsadhu562@gmail.com>
2025-05-25 10:43:59 +0530

    Add Auth Logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,34 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { RegistrationPage } from './pages/RegistrationPage';
 import { LoginPage } from './pages/LoginPage';
 import { HomePage } from './pages/HomePage';
 import { ForgotPassPage } from "./pages/ForgotPassPage";
-import './App.css'
 import VerifyEmail from "./Components/AuthComponent/VerifyEmail";
+import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate('/HomePage');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path='/register' element={<RegistrationPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/forgetpassword' element={<ForgotPassPage />} />
-      <Route path='/VerifyEmail' element={<VerifyEmail />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgetpassword" element={<ForgotPassPage />} />
+      <Route path="/VerifyEmail" element={<VerifyEmail />} />
+      <Route path="/HomePage" element={<HomePage />} />
     </Routes>
-  )
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/VerifyEmailPage.jsx
@@ -0,0 +1,9 @@
+import VerifyEmail from "../Components/AuthComponent/VerifyEmail"
+
+export const LoginPage = () => {
+    return (
+        <div>
+            <VerifyEmail />
+        </div>
+    )
+}
