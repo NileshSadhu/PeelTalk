e4a34930fefb382c5e131a24ef5f9ed99736f493
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-06-15 22:05:21 +0530

    fixed route

--- backend/src/routes/chat.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler, Router } from "express";
-import { getMessages } from "../controllers/chat.controller";
-import { authenticate } from "../middleware/auth.middleware";
-
-export const chatRouter = Router();
-
-chatRouter.get("/messages/:conversationId",authenticate , getMessages as unknown as RequestHandler);
-
