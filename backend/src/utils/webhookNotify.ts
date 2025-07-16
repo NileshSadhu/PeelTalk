d11c30be492466a861c1d8c783bb848ad37128ed
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-07-16 20:41:14 +0530

    discord webhook modified

--- backend/src/utils/webhookNotify.ts
+++ backend/src/utils/webhookNotify.ts
@@ -1,12 +1,21 @@
 import axios from "axios";
 
+const webhooks = [
+    process.env.DISCORD_WEBHOOK_URL,
+    process.env.DISCORD_WEBHOOK_URL_2,
+    process.env.DISCORD_WEBHOOK_URL_3,
+    process.env.DISCORD_WEBHOOK_URL_4,
+];
 
 export const sendDiscordMessage = async (text: string) => {
-    try {
-        await axios.post(process.env.DISCORD_WEBHOOK_URL!, {
-        content: text, 
-        });
-    } catch (err) {
-        console.error("❌ Failed to send Discord message:", err);
+    for (const url of webhooks) {
+        if (!url) continue;
+        try {
+            await axios.post(url, { content: text });
+            console.log("✅ Discord message sent successfully");
+            break; 
+        } catch (error: any) {
+            console.error(`❌ Failed to send to webhook ${url}:`, error.message);
+        }
     }
 };
