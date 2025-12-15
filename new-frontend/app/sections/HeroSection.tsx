177a36680c6e23fc4fcc4a98b54faa6fefdbdb22
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2025-12-15 16:44:47 +0530

    Add Hero and Footer section

---
+++ new-frontend/app/sections/HeroSection.tsx
@@ -0,0 +1,35 @@
+import NavBar from "../components/NavBar";
+
+export default function HeroSection() {
+  return (
+    <div className="relative flex flex-col min-h-screen items-center p-5 bg-linear-to-b from-yellow-300 to-white">
+      <NavBar />
+
+      {/* Banana decorations */}
+      <img
+        src="left.png"
+        className="absolute top-80 right-75 w-12 h-12 animate-bounce"
+        alt="banana"
+      />
+      <img
+        src="right.png"
+        className="absolute top-80 left-75 w-12 h-12 animate-bounce"
+        alt="banana"
+      />
+
+      {/* Hero content */}
+      <div className="flex flex-1 flex-col justify-center items-center text-center z-10">
+        <h1 className="text-5xl font-bold text-amber-900">
+          Talk to strangers. Regret it instantly.
+        </h1>
+        <p className="mt-4 text-amber-800 max-w-xl">
+          Talk to weirdos, oversharers, and emotional time bombs all hiding
+          behind fruity usernames. It’s random. It’s raw. It’s PeelTalk.
+        </p>
+        <button className="bg-white px-5 py-2 mt-3 rounded-2xl shadow-md hover:bg-yellow-100">
+          Chat Now
+        </button>
+      </div>
+    </div>
+  );
+}
